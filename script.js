9535fcc9b6c7a02e8aac7aa680cb7a6f377be9d6
Zackeus Zetterberg <zackeua@gmail.com>
2026-03-14 02:24:02 +0100

    Remove unnessecary javascript, default to dark mode, update text

--- script.js
+++ script.js
@@ -1,66 +1,15 @@
-const toggle = document.getElementById("dark-toggle");
+const toggle = document.getElementById("light-toggle");
 
 toggle.onclick = () => {
 
-document.body.classList.toggle("dark");
+document.body.classList.toggle("light");
 
 localStorage.setItem(
 "theme",
-document.body.classList.contains("dark")
+document.body.classList.contains("light")
 );
 
 };
 
 if(localStorage.getItem("theme") === "true")
-document.body.classList.add("dark");
-
-
-async function loadRepoInfo() {
-
-const projects = document.querySelectorAll(".project");
-
-for (const project of projects) {
-
-const repo = project.dataset.repo;
-
-if (!repo) continue;
-
-const res = await fetch(
-`https://api.github.com/repos/zackeua/${repo}`
-);
-
-const data = await res.json();
-
-const meta = project.querySelector(".project-meta");
-
-meta.innerHTML = `
-<span class="language">
-<span class="lang-dot"
-style="background:${languageColor(data.language)}">
-</span>
-${data.language ?? ""}
-</span>
-`;
-
-}
-
-}
-
-loadRepoInfo();
-
-
-function languageColor(lang){
-
-const colors = {
-
-"Python":"#3572A5",
-"C++":"#f34b7d",
-"Java":"#b07219",
-"JavaScript":"#f1e05a",
-"C":"#555"
-
-};
-
-return colors[lang] || "#888";
-
-}
+document.body.classList.add("light");
